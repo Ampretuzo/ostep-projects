99d2aa22502f42e40f557c332788e0c6e199e9fa
Sergi Chalauri <sergi.chalauri@gmail.com>
2021-04-15 23:36:51 +0400

    mystrstr

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -1,14 +1,31 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
 
 char *mystrstr(const char *haystack, const char *needle) {
     /*
-     * TODO: Reimplement strstr.
+     * Reimplement strstr.
      * For fun!
      */
+    long h_len = (long) strlen(haystack);
+    long n_len = (long) strlen(needle);
+
+    for (int i = 0; i < h_len - n_len; i++) {
+        bool match = true;
+        for (int j = 0; j < n_len; j++) {
+            if (haystack[i + j] != needle[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
+            return (char *) &haystack[i];
+        }
+    }
+    
     return NULL;
 }
 
@@ -20,7 +37,7 @@ void print_matches(FILE *input, char *searchterm) {
 
     // TODO: Handle getline errors after while
     while ((len_read = getline(&line, &len, input)) != -1) {
-        if (strstr(line, searchterm)) {
+        if (mystrstr(line, searchterm)) {
             printf(line);
         }
     }
