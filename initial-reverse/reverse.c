d9711a47a350e858a77ee209a3ff29b12632d360
Sergi Chalauri <sergi.chalauri@gmail.com>
2021-05-01 19:36:55 +0400

    Some file checks

--- initial-reverse/reverse.c
+++ initial-reverse/reverse.c
@@ -1,26 +1,69 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <sys/stat.h>
+
+
+#define fsame(stat1, stat2) (stat1.st_dev == stat2.st_dev && stat1.st_ino == stat2.st_ino)
 
 
 /* TODO: Error handling where applicable
  */
 int main(int argc, char *argv[]) {
+	if (argc < 2 || argc > 3) {
+		fprintf(stderr, "usage: reverse input [output]\n");
+		return 1;
+	}
+
 	char *input_filename;
+	char *output_filename;
 	FILE *input_file;
+	FILE *out;
 
 	input_filename = argv[1];
+
+	if (argc > 2) {
+		struct stat input_stat;
+		struct stat output_stat;
+		output_filename = argv[2];
+		if(stat(input_filename, &input_stat)) {
+			fprintf(stderr, "reverse: cannot open file '%s'\n", input_filename);
+			return 1;
+		}
+		if(!stat(output_filename, &output_stat)) {
+			if (fsame(input_stat, output_stat)) {
+				fprintf(stderr, "reverse: input and output files must differ\n");
+				return 1;
+			}
+		}
+	}
+
 	input_file = fopen(input_filename, "r");
+	if (!input_file) {
+		fprintf(stderr, "reverse: cannot open file '%s'\n", input_filename);
+		return 1;
+	}
+
+	out = stdout;
+	if (output_filename) {
+		out = fopen(output_filename, "w");
+		if (!out) {
+			fprintf(stderr, "reverse: cannot open file '%s'\n", output_filename);
+			return 1;
+		}
+	}
 
 	char *line = NULL;
 	size_t line_len = 0;
 	fpos_t fpos_start;
+
+	// Actual program:
 
 	fseek(input_file, 0, SEEK_END);
 
 	while (true) {
 		fgetpos(input_file, &fpos_start);
 		getline(&line, &line_len, input_file);
-		fprintf(stdout, "%s", line);
+		fprintf(out, "%s", line);
 		fsetpos(input_file, &fpos_start);
 
 		// Position at the start of the previous line
